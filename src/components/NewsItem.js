5f7c823b2cb2dab9a588cd9819f6aa0aeaf7a06a
royalmonteiro7 <royalmonteiro7@gmail.com>
2023-07-29 12:12:43 +0530

    first

---
+++ src/components/NewsItem.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div>
+          {" "}
+          <span
+            className="badge rounded-pill bg-danger"
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            {source}
+          </span>
+        </div>
+
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://static.themoscowtimes.com/image/og/37/81778__37b44f571a4697022bab0f733e150fb7.jpg"
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
